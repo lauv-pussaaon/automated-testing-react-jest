93e2850aeb3b1901fc8aaa068a79870f46df91d6
Lauv <pussaaon@gmail.com>
2024-03-04 17:17:11 +0700

    more complex test

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -1,7 +1,7 @@
-import { toUpperCase } from "../app/utils";
+import { getStringInfo, toUpperCase } from "../app/utils";
 
 describe("utils test suite", () => {
-    it("shouls return uppercase", () => {
+    it("should return uppercase", () => {
         // arrange system
         const sut = toUpperCase;
         const expected = "ABC";
@@ -11,5 +11,35 @@ describe("utils test suite", () => {
 
         // assert
         expect(actual).toBe("ABC");
+    });
+
+    it.only("should return info for valid string", () => {
+        const sut = getStringInfo;
+
+        const actual = sut("My-String");
+
+        expect(actual.lowerCase).toBe("my-string");
+        expect(actual.extraInfo).toEqual({});
+
+        expect(actual.characters.length).toBe(9);
+        expect(actual.characters).toHaveLength(9);
+
+        expect(actual.characters).toEqual([
+            "M",
+            "y",
+            "-",
+            "S",
+            "t",
+            "r",
+            "i",
+            "n",
+            "g",
+        ]);
+        expect(actual.characters).toContain<string>("M");
+
+        expect(actual.extraInfo).not.toBe(undefined);
+        expect(actual.extraInfo).not.toBeUndefined();
+        expect(actual.extraInfo).toBeDefined();
+        expect(actual.extraInfo).toBeTruthy();
     });
 });
