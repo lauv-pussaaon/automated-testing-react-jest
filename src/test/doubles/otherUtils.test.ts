467c75c05de8208c707f4db221aebd978476aa86
Lauv <pussaaon@gmail.com>
2024-03-06 17:40:44 +0700

    unit test - track function calls

--- src/test/doubles/otherUtils.test.ts
+++ src/test/doubles/otherUtils.test.ts
@@ -4,6 +4,39 @@ import {
 } from "../../app/doubles/otherUtils";
 
 describe("OtherUtils test suite", () => {
+    describe.only("Tracking callbacks with Jest func mock", () => {
+        const callBackMock = jest.fn();
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it("calls callback for invalid argument - track calls", () => {
+            const actual = toUpperCaseWithCallback("", callBackMock);
+            expect(actual).toBeUndefined();
+            expect(callBackMock).toHaveBeenCalledWith("Invalid argument!");
+            expect(callBackMock).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe.only("Tracking callbacks", () => {
+        let timesCalled = 0;
+
+        function callBackMock(arg: string) {
+            timesCalled++;
+        }
+
+        afterEach(() => {
+            timesCalled = 0;
+        });
+
+        it("calls callback for invalid arg", () => {
+            const actual = toUpperCaseWithCallback("", callBackMock);
+            expect(actual).toBeUndefined();
+            expect(timesCalled).toBe(1);
+        });
+    });
+
     describe("toUpperCaseWithCallback test suite", () => {
         it("calls callback for invalid arg", () => {
             const actual = toUpperCaseWithCallback("", () => {});
